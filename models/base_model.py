f9f78422d4af2d4c63c02acf3947594a70bc364c
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2022-01-27 11:24:13 +0200

    added save and to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Defines a class that will be the base for all the classes to be created in the AirBnB module.
+Defines a class that will be the base for all the classes to
+be created in the AirBnB module.
 """
 import models
 from uuid import uuid4
@@ -34,3 +35,30 @@ class BaseModel:
                     self.__dict__[key] = value
             else:
                 models.storage.new(self)
+
+    def __str__(self):
+        """
+            Returns the string representation of BaseModel
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                                                        self.id, self.__dict__)
+        
+    def save(self):
+        """
+            Updates the public instance attribute 'updated_at' with the current time
+        """
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+            Returns the dictionary representation of the object
+        """
+        obj = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                obj[key] = value.isoformat()
+            else:
+                obj[key] = value
+        obj["__class__"] = self.__class__.__name__
+        return obj
